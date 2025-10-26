07056043316b7e253ed882bdce20bdb69622330f
LeDinhChinh0812 <chinhledai1@gmail.com>
2025-10-27 01:37:29 +0700

    Lastest

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 
 
 const server = http.createServer((req, res) => {
-res.end(`Tôi đã build xong Jenkins! Build time: ${new Date().toISOString()}\n`);
+res.end(`Hello Jenkins! Build time: ${new Date().toISOString()}\n`);
 });
 
 
