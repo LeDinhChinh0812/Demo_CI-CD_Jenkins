e516c0a2eaa751e3b16995e4f1c131bf3f5e9f4d
LeDinhChinh0812 <chinhledai1@gmail.com>
2025-10-27 13:33:59 +0700

    New update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 
 
 const server = http.createServer((req, res) => {
-res.end(`Hello Jenkins !!!!! Build time: ${new Date().toISOString()}\n`);
+res.end(`Hello Jenkins hahahaha Build time: ${new Date().toISOString()}\n`);
 });
 
 
